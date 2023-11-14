4aac2f116eb9b14164cd7eb062f19bb5223a3ee5
Visar Sabani <vissarshabani@gmail.com>
2023-11-14 16:40:19 +0000

    Wikinimous - functionality without CSS

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  Rails.application.routes.draw do
-    resources :article
-  end
+  resources :articles
 end
